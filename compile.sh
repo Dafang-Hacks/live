e0fad6dc547592c03e16c6418d4555a377d2a535
jmtatsch <jmtatsch@users.noreply.github.com>
2019-10-17 12:40:11 +0200

    Update compile.sh
    fix directory level

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
+
 TOOLCHAIN=$(pwd)/../toolchain/bin
 CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
+
 export CC=${CROSS_COMPILE}gcc
 export LD=${CROSS_COMPILE}ld
 export CFLAGS="-muclibc -O2 -DDEBUG_TRACE -DFAKE_ROOT "
 export CPPFLAGS="-muclibc -O2"
 export LDFLAGS="-muclibc -O2 -g"
 
-
-cd live/
-./genMakefiles dafang
 make clean
+./genMakefiles dafang
 make
